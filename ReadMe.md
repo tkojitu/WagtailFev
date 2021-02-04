2a415a8e215312bb93f1ce8d050d5af8803b3379
tko <tko@jitu.org>
2021-02-04 22:13:29 +0900

    initial.

---
+++ ReadMe.md
@@ -0,0 +1,12 @@
+# WagtailFev
+
+WagtailFev records body temperature.
+
+## Spec and Design
+
+* The app has a editText to input body temperature.
+* The app saves body temperature in a private file at onStop.
+* The app adds timestamp to a record.
+* The app has a button to export records into clipboard.
+* The app has a clear menu to clear records.
+* The app has a textView to show the last temperature.
---
+++ app/src/main/java/com/example/wagtailfev/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.wagtailfev;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_clear) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
