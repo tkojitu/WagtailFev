5424c0ed6d94d0ca84a0fd1ea7bed7d794aeadac
tko <tko@jitu.org>
2021-02-04 23:22:07 +0900

    read/write file.

--- app/src/main/java/com/example/wagtailfev/MainActivity.java
+++ app/src/main/java/com/example/wagtailfev/MainActivity.java
@@ -6,8 +6,22 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String FILENAME = "wagtailfev.txt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +40,56 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         if (item.getItemId() == R.id.action_clear) {
+            deleteFile();
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        String str = readFile();
+        writeFile(str + "37.0\n");
+    }
+
+    private String readFile() {
+        StringBuilder buf = new StringBuilder();
+        try {
+            FileInputStream fis = openFileInput(FILENAME);
+            InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+            try (BufferedReader reader = new BufferedReader(isr)) {
+                String line = reader.readLine();
+                while (line != null) {
+                    buf.append(line).append('\n');
+                    line = reader.readLine();
+                }
+                return buf.toString();
+            } catch (IOException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+                return buf.toString();
+            }
+        } catch (FileNotFoundException e) {
+            return "";
+        }
+    }
+
+    private void writeFile(String str) {
+        try {
+            FileOutputStream fos = openFileOutput(FILENAME, MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos, StandardCharsets.UTF_8);
+            try (BufferedWriter writer = new BufferedWriter(osw)) {
+                writer.write(str);
+            } catch (IOException e) {
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        } catch (FileNotFoundException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void deleteFile() {
+        new File(getFilesDir(), FILENAME).delete();
     }
 
 }
